8321dc8c892bb2c8aa3590f66bd70493e522445a
Garrett Ashton Brady <nobody@nowhere.org>
2025-02-12 17:31:34 -0500

    Added data, figures, temp directories. Added CSV, DataFrames, Pluto, Tables, Missing pkgs to Manifest & Project. Created StellarDashboard.jl, which will serve as the main data analysis location

---
+++ StellarDashboard.jl
@@ -0,0 +1,26 @@
+### A Pluto.jl notebook ###
+# v0.20.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3a9c8be6-e990-11ef-044c-57e8b712c0fb
+#=
+Garrett Brady - gab5654@psu.edu
+
+Start: Feb 12th, 2025
+
+This notebook is the dashboard project for ASTRO-416
+Goals: 
+- Ingest SDSS DR17 MaSTAR data
+	- Focus on Star Clusters
+		- Write good ADQL queries for SDSS to get cleaner data
+- fill in the rest later...
+=#
+
+# ╔═╡ 1ad89925-9707-497f-b682-9f509910d361
+
+
+# ╔═╡ Cell order:
+# ╠═3a9c8be6-e990-11ef-044c-57e8b712c0fb
+# ╠═1ad89925-9707-497f-b682-9f509910d361
